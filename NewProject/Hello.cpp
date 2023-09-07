52ddf9dc62813450feeca113a0ac66eb91701130
minsung <minsung@DESKTOP-G58KEO3>
2023-09-07 20:01:16 +0900

    초기 커밋.

---
+++ NewProject/Hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class LetDebug
+{
+public:
+	void printNum()
+	{
+		int s1 = 32767;
+		int s2 = 1;
+		int s3 = s1 + s2;
+
+		std::cout << s3 << std::endl;
+	}
+};
+
+int main()
+{
+	LetDebug* Id = new LetDebug;
+
+	Id->printNum();
+
+	return 0;
+}
